80e2fd9aa14d34284d3a54193f8bee655ff05d7c
BB <ben@breakpointingbad.com>
2024-02-10 22:49:25 +0000

    ATIP: Fixed ATIP code.

--- virt-lab/edgers/attack-client/client-to-mitm/src/start-full-attack.sh
+++ virt-lab/edgers/attack-client/client-to-mitm/src/start-full-attack.sh
@@ -2,4 +2,4 @@
 
 
 # sike wrong number of args ---> (public_iface, attacker_pub_ip, victim_ip, vpn_ip, vpn_port, https_port, webdnsserver_ip)
-sudo /vagrant/client-to-mitm/src/full-relay enp0s8 192.168.4.254 192.168.1.254 192.168.2.254 1194 80 192.168.3.254
+sudo /vagrant/client-to-mitm/src/full-relay enp0s8 192.168.254.254 192.168.1.254 192.168.2.254 1194 80 192.168.3.254
