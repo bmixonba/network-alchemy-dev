43750c8e456f4b47064982aa69f1712a8c2904ca
BB <ben@breakpointingbad.com>
2024-02-10 18:42:38 +0000

    Fixed VPNserver configuration.

--- virt-lab/routers/router2/setup_net.sh
+++ virt-lab/routers/router2/setup_net.sh
@@ -13,7 +13,6 @@ echo "net.ipv4.conf.lo.rp_filter=0" >> /etc/sysctl.conf
 echo "net.ipv4.conf.enp0s3.rp_filter=0" >> /etc/sysctl.conf
 echo "net.ipv4.conf.enp0s8.rp_filter=0" >> /etc/sysctl.conf
 echo "net.ipv4.conf.enp0s9.rp_filter=0" >> /etc/sysctl.conf
-echo "net.ipv4.conf.enp0s10.rp_filter=0" >> /etc/sysctl.conf
 
 sysctl -p
 
