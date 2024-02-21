500fd00856eca096fec950482442afa64f25be47
BB <ben@breakpointingbad.com>
2024-02-21 22:49:50 +0000

    Fixing NAT.

--- virt-lab/edgers/attack-client/attacker/add_dnat.sh
+++ virt-lab/edgers/attack-client/attacker/add_dnat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Attacker's private ip
-INET_IP=10.8.0.14
+INET_IP=10.8.0.6
 
 # Actual websites IP address
 BB_IP=149.28.240.117
@@ -10,5 +10,11 @@ BB_IP=149.28.240.117
 LAN_IP=192.168.254.254
 
 
-iptables -t nat -A PREROUTING --dst $INET_IP -p tcp --dport 80 -j DNAT --to-destination $BB_IP
-iptables -t nat -A POSTROUTING -p tcp --dst $BB_IP --dport 80 -j SNAT --to-source $LAN_IP
+# Usually I put this after
+# iptables -t nat -$1 POSTROUTING -o tun0 -j MASQUERADE
+# iptables -t nat -$1 POSTROUTING -o enp0s8 -j MASQUERADE
+
+iptables -t nat -$1 PREROUTING -p tcp --dport 80 -j DNAT --to-destination $BB_IP:80
+# iptables -t nat -A POSTROUTING -s $BB_IP -j SNAT --to-destination 192.168.1.254 
+# Not sure this rule is needed.
+# iptables -t nat -$1 POSTROUTING -p tcp -s $BB_IP --dport 80 -j MASQUERADE 
