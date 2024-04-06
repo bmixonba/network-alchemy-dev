791986d9c09d2a0101eb73215a3d591fa05bc65c
BB <ben@breakpointingbad.com>
2024-04-06 15:09:48 +0000

    Added sources for software installation instructions.

--- virt-lab/install_deps.sh
+++ virt-lab/install_deps.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+
+# Install vagrant
+# Source: https://developer.hashicorp.com/vagrant/install
 sudo apt update && sudo apt upgrade
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install vagrant
 
 
+# Install virtualbox
+# Source: https://tecadmin.net/how-to-install-virtualbox-on-ubuntu-22-04/
 sudo apt-add-repository "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib"
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 sudo apt-get update
